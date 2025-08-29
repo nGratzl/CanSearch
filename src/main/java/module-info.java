839acca77e0c3e78661eb2ca0f3017ef6c34f041
nGratzl <nils.gratzl@grg17geblergasse.at>
2025-08-29 11:10:19 +0200

    Delete unused code

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module your.module.name {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    exports at.cansearch;
-    // If you use FXML and controllers, you may also need:
-    opens at.cansearch to javafx.fxml;
-}
